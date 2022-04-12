6127156c059d48660c01b938c91a1adc9731bde5
Daun1309 <100025580+Daun1309@users.noreply.github.com>
2022-04-13 00:05:15 +0900

    이동

--- mini/src/components/Header.js
+++ mini/src/components/Header.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Button, Grid, Text, Image } from "../elements";
-//import { useHistory } from 'react-router-dom';
+import { history } from "../redux/configureStore";
 
 const Header = () => {
-  //const history = useHistory();
-
+  
+  
   return (
     <React.Fragment>
       <Grid is_flex padding="4px 16px">
@@ -15,8 +15,11 @@ const Header = () => {
           <Image src= "https://ifh.cc/g/nn7mf5.png" />
         </Grid>
         <Grid is_flex>
-          <Button text="로그인" _onClick={() => {}}></Button>
-          <Button text="회원가입" _onClick={() => {}}></Button>
+        
+          <Button text="로그인" _onClick={(event) => { history.push("/login"); event.stopPropagation(); }} ></Button>
+          <Button text="회원가입" _onClick={() => {
+            history.push("/signup");
+          }}></Button>
         </Grid>
       </Grid>
     </React.Fragment>
