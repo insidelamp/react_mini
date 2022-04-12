cc29e60232686e4bcd606ccb58a9e7fb293d7d27
insidelamp <rkdhfh@naver.com>
2022-04-12 17:04:04 +0900

    이미지하는중

--- mini/src/pages/PostAdd.js
+++ mini/src/pages/PostAdd.js
@@ -1,23 +1,54 @@
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import React, { useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { actionCreators as postActions } from "../redux/modules/post";
 import { Grid, Button, Input, Text } from "../elements";
 import { history } from "../redux/configureStore";
+import Upload from "../shared/Upload";
 
-const PostWrite = (props) => {
-  console.log(props);
+const PostAdd = (props) => {
   const dispatch = useDispatch();
+  const fileInput = useRef();
   const [content, setContent] = React.useState("");
+  const preview = useSelector((state) => state.image.preview);
   const changeContents = (e) => {
     setContent(e.target.value);
   };
 
-  const addPost = () => {
-    dispatch(postActions.addPostDB({ content: content }), history.replace("/"));
+  const addPostBtn = () => {
+    const imageForm = new FormData();
+    console.log(imageForm);
+    let image = fileInput.current.files[0];
+    console.log(image);
+    imageForm.append("posts", image);
+    dispatch(
+      postActions.addPostDB({ content: content, imgURL: imageForm }),
+      history.replace("/")
+    );
   };
+
+  //   const postBtn = () => {
+  //     // console.log("버튼 클릭!");
+
+  //     const tagResult = tagData.filter((item, i) => item !== '');
+  //     if(tagResult.length === 0){window.alert('한개이상의 태그 작성해주세요~'); return;}
+
+  //     const imageForm = new FormData();
+  //     let image = fileInput.current.files[0];
+  //     imageForm.append('image', image);
+
+  //     // dispatch(imageActions.uploadImageFB(imageForm));
+
+  //     dispatch(postActions.addPostFB(title, tagResult, imageForm));
+
+  // }
 
   return (
     <React.Fragment>
+      <Grid padding="16px">
+        <Text margin="0px 0px 10px 10px">jpg, jpeg, png, gif 업로드 가능</Text>
+        <Upload preview={preview} _ref={fileInput} />
+        {/* <Image shape="rectangle" src={preview ? preview : "https://wpi.digication.com/srvs/filemanager/campus/jDt5abnGTNWMW3zpvKz5/resize=fit:crop,align:center,width:1182,height:667/compress/cache?access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnQiOiJjYW1wdXMiLCJrZXkiOiJqRHQ1YWJuR1ROV01XM3pwdkt6NSIsImV4cCI6OTk5OTk5OTk5OX0.UJ6s9UfmkeztKB_VajDR7LD1aOvLSrtPLz-gfi5I2_M"}></Image> */}
+      </Grid>
       <Grid>
         <Grid padding="16px">
           <Text margin="0px" size="36px" bold>
@@ -45,10 +76,10 @@ const PostWrite = (props) => {
       </Grid>
 
       <Grid padding="16px">
-        <Button text="게시글 작성" _onClick={addPost}></Button>)
+        <Button text="게시글 작성" _onClick={addPostBtn}></Button>)
       </Grid>
     </React.Fragment>
   );
 };
 
-export default PostWrite;
+export default PostAdd;
---
+++ mini/src/redux/modules/image.js
@@ -0,0 +1,79 @@
+import { createAction, handleActions } from "redux-actions";
+import produce from "immer";
+import axios from "axios";
+
+const UPLOADING = "UPLOADING";
+const UPLOAD_IMAGE = "UPLOAD_IMAGE";
+const SET_PREVIEW = "SET_PREVIEW";
+
+const uploading = createAction(UPLOADING, (uploading) => ({ uploading }));
+const uploadImage = createAction(UPLOAD_IMAGE, (image_url) => ({ image_url }));
+const setpreview = createAction(SET_PREVIEW, (preview) => ({ preview }));
+
+const initialState = {
+  image_url:
+    "https://wpi.digication.com/srvs/filemanager/campus/jDt5abnGTNWMW3zpvKz5/resize=fit:crop,align:center,width:1182,height:667/compress/cache?access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnQiOiJjYW1wdXMiLCJrZXkiOiJqRHQ1YWJuR1ROV01XM3pwdkt6NSIsImV4cCI6OTk5OTk5OTk5OX0.UJ6s9UfmkeztKB_VajDR7LD1aOvLSrtPLz-gfi5I2_M",
+  uploading: false,
+  preview: null,
+};
+
+const uploadImageFB = (imageForm) => {
+  console.log(uploadImageFB);
+  return function (dispatch, getState, { history }) {
+    dispatch(uploading(true));
+
+    // console.log(imageForm);
+
+    axios
+      .post(
+        "http://localhost:3001/posts",
+        imageForm // 미리 약속한 주소
+
+        // 서버가 필요로 하는 데이터를 넘겨주고,
+        // {
+        //     headers: {
+        //         'Content-Type': 'multipart/form-data'
+        //     },
+        // } // 누가 요청했는 지 알려줍니다. (config에서 해요!)
+      )
+      .then(function (res) {
+        console.log("upload response !! ", res);
+
+        // dispatch(uploadImage(`http://14.45.204.153:8080/${res.data}`));
+        dispatch(uploadImage(`http://localhost:3001/posts`));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+};
+
+export default handleActions(
+  {
+    [UPLOAD_IMAGE]: (state, action) =>
+      produce(state, (draft) => {
+        draft.image_url = action.payload.image_url;
+        draft.uploading = false;
+      }),
+
+    [UPLOADING]: (state, action) =>
+      produce(state, (draft) => {
+        draft.uploading = action.payload.uploading;
+      }),
+
+    [SET_PREVIEW]: (state, action) =>
+      produce(state, (draft) => {
+        draft.preview = action.payload.preview;
+      }),
+  },
+  initialState
+);
+
+const actionCreators = {
+  uploadImage,
+  uploadImageFB,
+  setpreview,
+  uploading,
+};
+
+export { actionCreators };
